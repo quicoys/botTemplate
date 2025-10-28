b92e7681d30f462291440fdd5dcca551a51389ff
quicoys <153209308+quicoys@users.noreply.github.com>
2025-10-28 17:32:17 +0100

    Refinement
    Added message logging to console & cleared stuff up a little

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const mineflayer = require('mineflayer');
-//const axios = require('axios'); //
+//const axios = require('axios'); // for API calls
 const { number } = require('yargs');
 
 function getRandomInt(max) {
@@ -10,10 +10,7 @@ function getAllArgs(args) {
     return getArgs(args, 1);
 }
 
-
 function getArgs(args, startIndex) {
-
-
     output = '';
     for (let index = startIndex; index < args.length; ++index) {
         output += args[index];
@@ -23,7 +20,6 @@ function getArgs(args, startIndex) {
 }
 
 const greetings = ["Hello", "Hi", "Yo", "Wazzup", "Howdy", "Hola"];
-
 
 console.log('--------------------------------------------------------------------');
 console.log('-                    quicbot systems: bot template.                -');
@@ -42,11 +38,11 @@ const initBot = () => {
 
   const prefix = '!';
   const masterPrefix = '$';
-  const admin = 'quicoys' //you can replace "quicoys" with your username
+  const admin = 'quicoys' //you can replace "quicoys" with your controller username
 
   bot.on('end', (reason) => {
       console.log(`[${bot.username}] Disconnected. Reason: ${reason}`);
-      setTimeout(initBot, 5000); //reconnect.
+      setTimeout(initBot, 5000); //reconnect after 5 seconds
   });
 
   bot.on('login', () => {
@@ -56,22 +52,22 @@ const initBot = () => {
 
   bot.on('chat', async (username, message) => {
       const currentDate = new Date().toLocaleString();
+      console.log(`${currentDate} | ${username}: {message}`);
 
       const msg = message.slice(1);
       const args = msg.split(' ');
-    if (username === admin ) {
-
+      
+    if (username === admin) {
         if (args[0].toLowerCase() === 'repeat') {
             if (args[1]) {
                 output = getAllArgs(args);
                 bot.chat(`> ${output}`);
             }
-        }
+         }
       }
 
       if (message.startsWith(prefix)) {
           if (!args[0]) return;
-
 
           if (args[0].toLowerCase().startsWith('mindreader')) {
             if (args.length < 2) {
@@ -81,13 +77,11 @@ const initBot = () => {
                 bot.chat(`are you dumb? input a NUMBER, 1-10.`)
                 return;
             }
-            bot.chat(`Thinking... Analysing... Reasoning... This may take a few seconds.`)
+            bot.chat(`Thinking... Analyzing... Reasoning... This may take a few seconds.`)
 
             setTimeout(() => {
                 bot.chat(`${username}; was your number ${args[1]}?`)
-            }, (Math.random() * 8000) + 2000);
-        
-
+            }, (Math.random() * 8000) + 2000) // 2-10 second delay
          }
 
           if (args[0].toLowerCase().startsWith('test')) {
@@ -95,7 +89,7 @@ const initBot = () => {
                 bot.chat('Hey! this is an example command. It can do anything you want!')
                 console.log(Math.random())
                 console.log('')
-                console.log('Online players on:' + botArgs['host'])
+                console.log('Online players on: ' + botArgs['host'] + ': ' + Object.keys(bot.players).length)
                 setTimeout(() => {
                     bot.chat(`my admin is: ${admin}, you are ${username}, the time in unix timestamp is: ${Date.now()} and I am: ${bot.username}`)
                 }, 2000 + Math.random() * 100); //two seconds + a number from 1 to 0 times 100 (+-50ms)
@@ -110,7 +104,7 @@ const initBot = () => {
                     }
                 }, 2000);
             }
-        }
+         } 
       }
   });
 
