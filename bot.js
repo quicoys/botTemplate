d0b3724b3e94003d1e466b5a407616088c757005
quicoys <153209308+quicoys@users.noreply.github.com>
2026-09-04 17:41:02 +0200

    Fix paranthesis
    Made the code actually run (im retarded)

--- bot.js
+++ bot.js
@@ -57,57 +57,59 @@ const initBot = () => {
       const msg = message.slice(1);
       const args = msg.split(' ');
       
-    if (username === admin) {
-        if (args[0].toLowerCase() === 'repeat') {
-            if (args[1]) {
-                output = getAllArgs(args);
-                bot.chat(`> ${output}`);
+        if (username === admin) {
+            if (args[0].toLowerCase() === 'repeat') {
+                if (args[1]) {
+                    output = getAllArgs(args);
+                    bot.chat(`> ${output}`);
+                }
             }
-         }
-      }
+        }
 
-      if (message.startsWith(prefix)) {
-          if (!args[0]) return;
+        if (message.startsWith(prefix)) {
+            if (!args[0]) return;
 
-          if (args[0].toLowerCase().startsWith('mindreader')) {
-            if (args.length < 2) {
-                bot.chat(`this is a super advanced program that can read your mind. Please use ${prefix}mindreader <number 1-10> to experience this advanced application.`)
-                return;
-                const num = Number(args[1]);
-                if (isNaN(num) || num < 1 || num > 10) {
-                    bot.chat(`are you dumb? input a NUMBER, 1-10.`)
+            if (args[0].toLowerCase().startsWith('mindreader')) {
+                if (args.length < 2) {
+                    bot.chat(`this is a super advanced program that can read your mind. Please use ${prefix}mindreader <number 1-10> to experience this advanced application.`)
                     return;
-                }
-            bot.chat(`Thinking... Analyzing... Reasoning... This may take a few seconds.`)
-
-            setTimeout(() => {
-                bot.chat(`${username}; was your number ${args[1]}?`)
-            }, (Math.random() * 8000) + 2000) // 2-10 second delay
-         }
-
-          if (args[0].toLowerCase().startsWith('test')) {
-            if (args.length < 2) {
-                bot.chat('Hey! this is an example command. It can do anything you want!')
-                console.log(Math.random())
-                console.log('')
-                console.log('Online players on: ' + botArgs['host'] + ': ' + Object.keys(bot.players).length)
-                setTimeout(() => {
-                    bot.chat(`my admin is: ${admin}, you are ${username}, the time in unix timestamp is: ${Date.now()} and I am: ${bot.username}`)
-                }, 2000 + Math.random() * 100); //two seconds + a number from 1 to 0 times 100 (+-50ms)
-                for (const userId in bot.players) {
-                        console.log(userId);
-                }
-            } else {
-                bot.chat('ooh, arguments! here you can handle what should happen if someone provides more input!')
-                setTimeout(() => {
-                    if (!isNaN(args[1])) {
-                        bot.chat('you provided a number.')
+                    const num = Number(args[1]);
+                    if (isNaN(num) || num < 1 || num > 10) {
+                        bot.chat(`are you dumb? input a NUMBER, 1-10.`)
+                        return;
                     }
-                }, 2000);
+                    bot.chat(`Thinking... Analyzing... Reasoning... This may take a few seconds.`)
+
+                    setTimeout(() => {
+                        bot.chat(`${username}; was your number ${args[1]}?`)
+                    }, (Math.random() * 8000) + 2000) // 2-10 second delay
+                }
             }
-         } 
-      }
-  });
+
+            if (args[0].toLowerCase().startsWith('test')) {
+                if (args.length < 2) {
+                    bot.chat('Hey! this is an example command. It can do anything you want!')
+                    console.log(Math.random())
+                    console.log('')
+                    console.log('Online players on: ' + botArgs['host'] + ': ' + Object.keys(bot.players).length)
+                    setTimeout(() => {
+                        bot.chat(`my admin is: ${admin}, you are ${username}, the time in unix timestamp is: ${Date.now()} and I am: ${bot.username}`)
+                    }, 2000 + Math.random() * 100); //two seconds + a number from 1 to 0 times 100 (+-50ms)
+                    for (const userId in bot.players) {
+                            console.log(userId);
+                    }
+                } else {
+                    bot.chat('ooh, arguments! here you can handle what should happen if someone provides more input!')
+                    setTimeout(() => {
+                        if (!isNaN(args[1])) {
+                            bot.chat('you provided a number.')
+                        }
+                    }, 2000);
+                }
+            } 
+        }
+    });
+    
 
   bot.on('kicked', (reason) => {
       let botSocket = bot._client.socket;
